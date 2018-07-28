d0ab2c42d03ae14a8ab6b62d6cb5589de8d3840d
Stax The Engipreneur <katlego@tshirelectro.co.za>
2018-07-28 12:56:35 +0200

    Exercise 4.1

--- src/screen/scr_main.cpp
+++ src/screen/scr_main.cpp
@@ -1,4 +1,8 @@
-// Exercising the Screen class
+//File_name: scr_main.cpp
+//Author:    Stax The Engipreneur
+//Date:      28 July 2018
+//Details:   Code uses Screen object to print letter K on 6by6 grid
+
 #include "screen.h"
 #include <iostream>
 
@@ -6,27 +10,47 @@ using namespace std;
 
 int main()
 {
-	auto myScreen = Screen{6,6};
-	myScreen.forward();
-	myScreen.set('*');
-	myScreen.down();
-	myScreen.set('*');
-	myScreen.move(3,3);
-	myScreen.set("---");
-
-	myScreen.display();
-	cout << endl;
-
-	myScreen.reSize(16,16);
-	myScreen.display();
-	myScreen.clear(' ');
-
-	myScreen.move(7,7);
-	myScreen.set("BIG");
-	myScreen.move(8,5);
-	myScreen.set("SCREEN");
-	myScreen.display();
-
-	return 0;
+    //declare a 6 by 6 screen with blank background
+    auto staxScreen = Screen{6,6,' '};
+    
+    //set current cursor location
+    //part 1: draw vertical line
+    for(int i = 0; i < 5; i++){
+        //write * at current location
+        //staxScreen.set('*');
+        //move cursor to next row
+        staxScreen.down();
+        //write * at current location
+        staxScreen.set('*');
+    }//end of part1
+    
+    //move cursor to 4,2
+    staxScreen.move(4,2);
+    staxScreen.set("**");
+    
+    //move cursor forward and down and place * there
+    for(int i = 0; i < 2;i++){
+        staxScreen.forward();
+        staxScreen.down();
+        staxScreen.set('*');
+    }//end of part 2
+    
+    //go back to 'center'
+    staxScreen.move(4,3);
+    
+    //move cursor forward and up and place * there
+    for(int i = 0; i < 2;i++){
+        staxScreen.forward();
+        staxScreen.up();
+        staxScreen.set('*');
+    }//end of part 2
+    
+    //finish off the letter k...
+    
+    //display the screen
+    staxScreen.display();
+    
+    //indicate that main terminated successfully
+    return 0;
 }
 
