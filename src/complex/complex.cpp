9459835a7a254bb8ac5cd1f9b65467b1ca20f469
Stax The Engipreneur <katlego@tshirelectro.co.za>
2018-07-28 12:45:59 +0200

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -5,26 +5,56 @@
 
 #include <iostream>	// contains the definition of cout, endl
 #include <complex>	// contains the complex class definition
+using std::sqrt;
+
+#include <cmath>
+using std::cout;
+using std::cin;
+using std::endl;
 
 using namespace std; // cout, endl, complex are all part of this namespace
 
 //simple to be used as substitute for complex<vector>
 using complexF = complex<float>;
-
+  
 int main()
 {
-    //we will replace complex<float> with simplex whenever we instantiate an object of complex type
-	auto num1 = complexF{2.0, 2.0};  // use auto for type deduction
-	auto num2 = complexF{4.0, -2.0}; // use uniform initialisation syntax (curly braces)
+    //initialize local variable
+    //coeffs..
+    complexF a,b,c;
+    //these two variables are used to ensure that we multiply complex numbers with complex numbers 
+    complexF four{4,0};
+    complexF two{2,0};
+    char reply;
+    //declare roots as complex, both uninitialized
+    complexF root1;
+    complexF root2;
+    complexF disc;
+    
+    //compute the following set of statements repeatedly until user quits
+    do{
+        //prompt user for input
+        cout << "Enter coefficients: ";
+        cin  >> a >> b >> c;
+        
+        disc = (b*b) - four*a*c;
+        
+        //compute roots of equation
+        root1 = (-b + sqrt(disc))/(two*a);
+        root2 = (-b + sqrt(disc)/(two*a));
+        
+        //display results
+        cout << "Roots of equation:\n";
+        cout << root1.real() << " + j" << root1.imag() << endl;
+        cout << root2.real() << " + j" << root2.imag() << endl;
+        
+        //prompt user for reply
+        cout << "\nPress q to quit|y to continue): " ;
+        cin  >> reply;
 
-	auto answer = num1 * num2; // type deduced for 'answer' is: complex<float>
+    }while(reply != 'q');
 
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
-
-	// answer++;
-
-	return 0;
+    
+    //indicate that main terminated successfully
+    return 0;
 }
